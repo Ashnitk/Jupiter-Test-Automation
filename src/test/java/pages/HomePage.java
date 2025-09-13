63bef03ae00cb8da0e44077acb2c5b8dbd6814da
ashnitkumar <ashnit.kumar@gmail.com>
2025-09-14 09:24:53 +1200

    Check in Cart and Shop pages. Update Home pages to use methods to access these pages.

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -7,7 +7,6 @@ public class HomePage {
     public final Page page;
     public final Locator WELCOME_MESSAGE;
 
-
     public HomePage(Page page){
         this.page = page;
         this.WELCOME_MESSAGE = page.getByText("Welcome to Jupiter Toys");
@@ -16,5 +15,13 @@ public class HomePage {
 
     public void clickContactPage() {
         page.click("role=link[name='Contact']");
+    }
+
+    public void clickShopPage() {
+        page.click("role=link[name='Shop']");
+    }
+
+    public void clickCartPage() {
+        page.click("#nav-cart");
     }
 }
---
+++ src/test/java/pages/ShopPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import com.microsoft.playwright.Page;
+
+public class ShopPage {
+
+    public final Page page;
+
+    public ShopPage(Page page) {
+        this.page = page;
+    }
+}
